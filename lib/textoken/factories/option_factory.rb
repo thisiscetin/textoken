3cc0ef8c1175299f0add89c1c10240b79bad9e38
Mehmet Cetin <mcetin.cm@gmail.com>
2015-09-26 23:08:31 +0300

    support 1.9.3

--- lib/textoken/factories/option_factory.rb
+++ lib/textoken/factories/option_factory.rb
@@ -13,9 +13,14 @@ module Textoken
     end
 
     def self.option_klass(key)
-      Object.const_get('Textoken::' + camelize(key), inherit: true)
+      Object.const_get(klass_name(key))
     rescue NameError
       Textoken.expression_error("#{key}: is not a valid option.")
+    end
+
+    def self.klass_name(key)
+      name = camelize(key)
+      RUBY_VERSION < "2.0.0" ? name.to_sym : 'Textoken::' + name
     end
   end
 end
