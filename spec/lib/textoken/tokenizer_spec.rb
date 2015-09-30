99021375aeb0b19fc23cb3a91a06af52d0cd8063
Mehmet Cetin <mcetin.cm@gmail.com>
2015-09-30 21:14:37 +0300

    fix typo

--- spec/lib/textoken/tokenizer_spec.rb
+++ spec/lib/textoken/tokenizer_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
 
 module TestDumy
+  # dumy class to mock findings
   class Dumy
     attr_reader :findings
 
     def initialize
       @findings = Findings.new
-    new
+    end
   end
 
+  # many punctuation types to be tested
   class Findings
-    def initialize
-    end
-
     def collection
       [[1, 'Alfa'], [5, 'beta\'s'], [6, 'teta?'], [7, 'omega !'], [9, 'psi.']]
     end
